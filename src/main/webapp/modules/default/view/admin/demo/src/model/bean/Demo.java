bee401fa5e52084e221c46677fed4917423b689f
wujun <wujun@joe-mac.local>
2016-06-18 14:00:17 +0800

    fix bug in create module.

--- src/main/webapp/modules/default/view/admin/demo/src/model/bean/Demo.java
+++ src/main/webapp/modules/default/view/admin/demo/src/model/bean/Demo.java
@@ -17,61 +17,70 @@ import com.mongodb.BasicDBObject;
 @DBMapping(collection = "tbldemo")
 public class Demo extends Bean {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
 
-	public String getId() {
-		return this.getString(X._ID);
-	}
+  public String getId() {
+    return this.getString(X._ID);
+  }
 
-	public String getName() {
-		return this.getString("name");
-	}
+  public String getName() {
+    return this.getString("name");
+  }
 
-	public String getContent() {
-		return this.getString("content");
-	}
+  public String getContent() {
+    return this.getString("content");
+  }
 
-	// ------------
+  // ------------
 
-	public static String create(V v) {
-		/**
-		 * generate a unique id in distribute system
-		 */
-		String id = "d" + UID.next("demo.id");
+  public static String create(V v) {
+    /**
+     * generate a unique id in distribute system
+     */
+    String id = "d" + UID.next("demo.id");
+    try {
+      while (exists(id)) {
+        id = "d" + UID.next("demo.id");
+      }
+      Bean.insert(v.set(X._ID, id), Demo.class);
+      return id;
+    } catch (Exception e1) {
+      log.error(e1.getMessage(), e1);
+    }
+    return null;
+  }
 
-		while (exists(id)) {
-			id = "d" + UID.next("demo.id");
-		}
-		Bean.insert(v.set(X._ID, id), Demo.class);
-		return id;
-	}
+  public static boolean exists(String id) {
+    try {
+      return Bean.exists(new BasicDBObject(X._ID, id), Demo.class);
+    } catch (Exception e1) {
+      log.error(e1.getMessage(), e1);
+    }
+    return false;
+  }
 
-	public static boolean exists(String id) {
-		return Bean.exists(new BasicDBObject(X._ID, id), Demo.class);
-	}
+  public static int update(String id, V v) {
+    return Bean.updateCollection(id, v, Demo.class);
+  }
 
-	public static int update(String id, V v) {
-		return Bean.updateCollection(id, v, Demo.class);
-	}
+  public static Beans<Demo> load(BasicDBObject q, int s, int n) {
+    return Bean.load(q, new BasicDBObject(X._ID, 1), s, n, Demo.class);
+  }
 
-	public static Beans<Demo> load(BasicDBObject q, int s, int n) {
-		return Bean.load(q, new BasicDBObject(X._ID, 1), s, n, Demo.class);
-	}
+  public static Demo load(String id) {
+    return Bean.load(new BasicDBObject(X._ID, id), Demo.class);
+  }
 
-	public static Demo load(String id) {
-		return Bean.load(new BasicDBObject(X._ID, id), Demo.class);
-	}
+  public static void delete(String id) {
+    Bean.delete(new BasicDBObject(X._ID, id), Demo.class);
+  }
 
-	public static void delete(String id) {
-		Bean.delete(new BasicDBObject(X._ID, id), Demo.class);
-	}
-
-	public static void cleanup() {
-		// TODO Auto-generated method stub
-		// just for demo
-	}
+  public static void cleanup() {
+    // TODO Auto-generated method stub
+    // just for demo
+  }
 
 }
