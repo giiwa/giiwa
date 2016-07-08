3908b70e3aeeb687d184f6dc900ea250f7ba32e7
wujun <wujun@joe-mac.local>
2016-07-08 08:06:15 +0800

    fix jsp issue in windows

--- src/main/java/org/giiwa/framework/web/view/JspView.java
+++ src/main/java/org/giiwa/framework/web/view/JspView.java
@@ -13,14 +13,16 @@ public class JspView extends View {
     String name = file.getCanonicalPath().substring(Model.HOME.length());
     log.debug("viewname=" + name);
 
+    name = name.replaceAll("\\\\", "/");
     m.req.setAttribute(System.getProperty("org.apache.jasper.Constants.JSP_FILE", "org.apache.catalina.jsp_file"),
         name);
+
     RequestDispatcher rd = m.req.getRequestDispatcher(name);
     if (rd == null) {
       log.warn("Not a valid resource path:" + name);
       return false;
     } else {
-      log.debug("rd=" + rd);
+      log.debug("including jsp page, name=" + name);
     }
     rd.include(m.req, m.resp);
 
--- src/main/webapp/modules/default/view/admin/clone/backup_db.sh
+++ src/main/webapp/modules/default/view/admin/clone/backup_db.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+#postgresql
 sudo -u postgres pg_dump $1 > $2
+
+#mysql
+#mysqldump -u root $1 > $2
