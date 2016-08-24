237c8a6c39cadee425b5a7990aa6eb79b1813f2a
wujun <wujun@joe-mac.local>
2016-08-25 07:02:09 +0800

    update the javadoc

--- src/main/java/org/giiwa/app/web/admin/gauge.java
+++ src/main/java/org/giiwa/app/web/admin/gauge.java
@@ -1,5 +1,5 @@
 /*/*
- * Copyright 2015 Giiwa, Inc. and/or its affiliates.
+ * Copyright 2015 JIHU, Inc. and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
