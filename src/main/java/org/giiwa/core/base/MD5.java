8d27c16cf575aeb3f26c10db2770f7d209f9f418
wujun <wujun@joe-mac.local>
2016-09-15 18:32:10 +0800

    add javadoc

--- src/main/java/org/giiwa/core/base/MD5.java
+++ src/main/java/org/giiwa/core/base/MD5.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright 2015 JIHU, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
 package org.giiwa.core.base;
 
 import java.io.File;
@@ -10,9 +24,17 @@ import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+// TODO: Auto-generated Javadoc
 public class MD5 {
   private static Log log = LogFactory.getLog(MD5.class);
 
+  /**
+   * Md5.
+   *
+   * @param f
+   *          the f
+   * @return the string
+   */
   public static String md5(File f) {
     FileInputStream fis = null;
     try {
@@ -31,6 +53,15 @@ public class MD5 {
     return null;
   }
 
+  /**
+   * Md5.
+   *
+   * @param fis
+   *          the fis
+   * @return the string
+   * @throws Exception
+   *           the exception
+   */
   public static String md5(InputStream fis) throws Exception {
     MessageDigest md = MessageDigest.getInstance("MD5");
 
@@ -43,10 +74,23 @@ public class MD5 {
     return new String(org.apache.commons.codec.binary.Hex.encodeHex(md.digest()));
   }
 
+  /**
+   * Md5.
+   *
+   * @param target
+   *          the target
+   * @return the string
+   */
   public static String md5(String target) {
     return DigestUtils.md5Hex(target);
   }
 
+  /**
+   * The main method.
+   *
+   * @param args
+   *          the arguments
+   */
   public static void main(String[] args) {
     File f = new File("/Users/wujun/d/workspace/giiwa/README.md");
     System.out.println(md5(f));
