14fb7c37076ee2fe2749c5bc47a95981f6acf6df
giiwa <yjiang@giisoo.com>
2016-10-05 08:24:41 +0800

    update the GiiwaFilter

--- src/main/java/org/giiwa/framework/web/GiiwaFilter.java
+++ src/main/java/org/giiwa/framework/web/GiiwaFilter.java
@@ -51,61 +51,66 @@ public class GiiwaFilter implements Filter {
   public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
       throws IOException, ServletException {
 
-    HttpServletRequest r1 = (HttpServletRequest) req;
-    HttpServletResponse r2 = (HttpServletResponse) resp;
+    try {
+      HttpServletRequest r1 = (HttpServletRequest) req;
+      HttpServletResponse r2 = (HttpServletResponse) resp;
 
-    String uri = r1.getRequestURI();
-    while (uri.indexOf("//") > -1) {
-      uri = uri.replaceAll("//", "/");
-    }
-
-    String method = r1.getMethod();
-
-    if ("GET".equalsIgnoreCase(method)) {
-      if (!X.isEmpty(cross_domain)) {
-        r2.addHeader("Access-Control-Allow-Origin", cross_domain);
+      String uri = r1.getRequestURI();
+      while (uri.indexOf("//") > -1) {
+        uri = uri.replaceAll("//", "/");
       }
 
-      Controller.dispatch(uri, r1, r2, new HTTPMethod(Model.METHOD_GET));
+      String method = r1.getMethod();
 
-    } else if ("POST".equalsIgnoreCase(method)) {
-      if (!X.isEmpty(cross_domain)) {
+      if ("GET".equalsIgnoreCase(method)) {
+        if (!X.isEmpty(cross_domain)) {
+          r2.addHeader("Access-Control-Allow-Origin", cross_domain);
+        }
+
+        Controller.dispatch(uri, r1, r2, new HTTPMethod(Model.METHOD_GET));
+
+      } else if ("POST".equalsIgnoreCase(method)) {
+        if (!X.isEmpty(cross_domain)) {
+          r2.addHeader("Access-Control-Allow-Origin", cross_domain);
+        }
+
+        Controller.dispatch(uri, r1, r2, new HTTPMethod(Model.METHOD_POST));
+
+      } else if ("OPTIONS".equals(method)) {
+        r2.setStatus(200);
         r2.addHeader("Access-Control-Allow-Origin", cross_domain);
+        r2.addHeader("Access-Control-Allow-Headers", cross_header);
+        r2.getOutputStream().write(cross_domain.getBytes());
+      } else if ("HEAD".equals(method)) {
+
       }
 
-      Controller.dispatch(uri, r1, r2, new HTTPMethod(Model.METHOD_POST));
-
-    } else if ("OPTIONS".equals(method)) {
-      r2.setStatus(200);
-      r2.addHeader("Access-Control-Allow-Origin", cross_domain);
-      r2.addHeader("Access-Control-Allow-Headers", cross_header);
-      r2.getOutputStream().write(cross_domain.getBytes());
-    } else if ("HEAD".equals(method)) {
-
+      // chain.doFilter(req, resp);
+    } catch (Throwable e) {
+      log.error(e.getMessage(), e);
     }
-
-    // chain.doFilter(req, resp);
-
   }
 
   @SuppressWarnings("rawtypes")
   @Override
   public void init(FilterConfig c1) throws ServletException {
+    try {
+      Model.s️ervletContext = c1.getServletContext();
 
-    Model.s️ervletContext = c1.getServletContext();
+      cross_domain = Global.getString("cross.domain", "no");
+      cross_header = Global.getString("cross.header", "forbidden");
 
-    cross_domain = Global.getString("cross.domain", "no");
-    cross_header = Global.getString("cross.header", "forbidden");
+      Enumeration e = c1.getInitParameterNames();
+      while (e.hasMoreElements()) {
+        String name = e.nextElement().toString();
+        String value = c1.getInitParameter(name);
+        config.put(name, value);
+      }
 
-    Enumeration e = c1.getInitParameterNames();
-    while (e.hasMoreElements()) {
-      String name = e.nextElement().toString();
-      String value = c1.getInitParameter(name);
-      config.put(name, value);
+      View.init(config);
+    } catch (Throwable e) {
+      log.error(e.getMessage(), e);
     }
-
-    View.init(config);
-
   }
 
   private Map<String, String> config = new HashMap<String, String>();
