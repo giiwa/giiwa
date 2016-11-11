2b81d8f6d4d3abd2d16f0f790707bd8fe8e550ed
giiwa <yjiang@giisoo.com>
2016-11-12 02:29:20 +0800

    linux startup shell, start and monitor the giiwa

--- tomcat/giiwa.sh
+++ tomcat/giiwa.sh
@@ -1,5 +1,56 @@
 #!/bin/sh
 
 echo "Starting giiwa ..."
-bin/startup.sh
 
+PRG="$0" 
+while [ -h "$PRG" ] ; do
+	ls=`ls -ld "$PRG"`
+	link=`expr "$ls" : '.*-> \(.*\)$'`
+	if expr "$link" : '/.*' > /dev/null; then
+		PRG="$link"
+	else
+		PRG=`dirname "$PRG"`/"$link"
+	fi
+done
+
+os=`uname -s`
+startup=`pwd`/bin
+
+getpid() {
+	case $os in
+	Linux)
+		pid=`ps fux |grep java | grep $1 |grep -v grep | awk '{print $2}'` 
+		;;
+	SunOs)
+		pid=`ps -gxww |grep java |grep $1 |grep -v grep | awk '{print $1}'`
+		;;
+	*)
+		return 2
+		;;
+	esac
+
+	return 0
+}
+
+start() {
+	getpid $startup
+	if [ -n "$pid" ]
+	then
+		echo ERROR: Appliction pid=$pid is still running
+		return 1
+	fi	
+	
+	$startup/startup.sh
+	return 0
+}
+
+while true; do
+	getpid $startup
+	if [ -n "$pid" ]
+	then
+		sleep 1
+	else
+		start
+		sleep 3
+	fi
+done
