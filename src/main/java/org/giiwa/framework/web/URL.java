45cef544f639d664380b32ac8164aebf1be69d4a
giiwa <yjiang@giisoo.com>
2016-11-14 11:00:57 +0800

    add url rewrite

--- src/main/java/org/giiwa/framework/web/URL.java
+++ src/main/java/org/giiwa/framework/web/URL.java
@@ -1,11 +1,8 @@
 package org.giiwa.framework.web;
 
-import java.util.HashMap;
-import java.util.Map;
+import org.giiwa.core.conf.Global;
 
 public class URL {
-
-  private static final Map<String, String> rewrites = new HashMap<String, String>();
 
   /**
    * force rewrite some url to new
@@ -16,14 +13,11 @@ public class URL {
    *          the new url
    */
   public static void rewrite(String originalurl, String newurl) {
-    rewrites.put(originalurl, newurl);
+    Global.setConfig("rewrite/" + originalurl, newurl);
   }
 
   public static String rewrite(String uri) {
-    if (rewrites.containsKey(uri)) {
-      return rewrites.get(uri);
-    }
-    return uri;
+    return Global.getString("rewrite/" + uri, uri);
   }
 
 }
