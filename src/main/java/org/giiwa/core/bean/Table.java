2e7cff89e17fdeb6066f9396a3d45ea7848ef24a
yuandm@foxmail.com <yuandm@foxmail.com>
2017-07-14 12:25:40 +0800

    Add inherited

--- src/main/java/org/giiwa/core/bean/Table.java
+++ src/main/java/org/giiwa/core/bean/Table.java
@@ -15,6 +15,7 @@
 package org.giiwa.core.bean;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -32,6 +33,7 @@ import java.lang.annotation.Target;
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
+@Inherited
 public @interface Table {
 
   /**
