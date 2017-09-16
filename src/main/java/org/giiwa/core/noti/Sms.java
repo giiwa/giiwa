8c7928f3ed5d036ab4463c99736e17186ae577aa
joe <joe@192.168.1.100>
2017-09-16 14:34:59 +0800

    update Sms.java

--- src/main/java/org/giiwa/core/noti/Sms.java
+++ src/main/java/org/giiwa/core/noti/Sms.java
@@ -26,64 +26,64 @@ import org.giiwa.framework.bean.OpLog;
  */
 public class Sms {
 
-  static final List<ISender> senders = new ArrayList<ISender>();
+	static final List<ISender> senders = new ArrayList<ISender>();
 
-  /**
-   * register a sms sender.
-   *
-   * @param seq
-   *          the lower first
-   * @param sender
-   *          the sender
-   */
-  public static void register(int seq, ISender sender) {
+	/**
+	 * register a sms sender.
+	 *
+	 * @param seq
+	 *            the lower first
+	 * @param sender
+	 *            the sender
+	 */
+	public static void register(int seq, ISender sender) {
 
-    if (sender != null && !senders.contains(sender)) {
-      senders.add(seq, sender);
-    }
+		if (sender != null && !senders.contains(sender)) {
+			senders.add(seq, sender);
+		}
 
-  }
+	}
 
-  /**
-   * send the sms.
-   *
-   * @param mobile
-   *          the mobile
-   * @param template
-   *          the internal template code
-   * @param jo
-   *          the jo
-   * @return true: success <br>
-   *         false: failed
-   */
-  public static boolean send(String mobile, String template, JSON jo) {
-    jo.put("template", template);
+	/**
+	 * send the sms.
+	 *
+	 * @param mobile
+	 *            the mobile
+	 * @param template
+	 *            the internal template code
+	 * @param jo
+	 *            the jo
+	 * @return true: success <br>
+	 *         false: failed
+	 */
+	public static boolean send(String mobile, String template, JSON jo) {
+		jo.put("template", template);
 
-    OpLog.info("sms", "send", jo.toString(), null, null);
+		OpLog.info("sms", "send", "mobile=" + mobile + ", jo=" + jo.toString(), null, null);
 
-    for (ISender s : senders) {
-      if (s.send(mobile, jo)) {
-        return true;
-      }
-    }
-    return false;
-  }
+		for (ISender s : senders) {
+			if (s.send(mobile, jo)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-  public static interface ISender {
+	public static interface ISender {
 
-    /**
-     * send sms.
-     *
-     * @param mobile
-     *          the mobile number of receiver
-     * @param jo
-     *          key value data <br>
-     *          sign: the sign of the sms <br>
-     *          template: the template code of the sms, e.g. registeruser,
-     *          forgetpassword <br>
-     * @return true: success <br>
-     *         false: failed
-     */
-    public boolean send(String mobile, JSON jo);
-  }
+		/**
+		 * send sms.
+		 *
+		 * @param mobile
+		 *            the mobile number of receiver
+		 * @param jo
+		 *            key value data <br>
+		 *            sign: the sign of the sms <br>
+		 *            template: the template code of the sms, e.g. registeruser,
+		 *            forgetpassword <br>
+		 * @return true: success <br>
+		 *         false: failed
+		 */
+		public boolean send(String mobile, JSON jo);
+	}
 }
