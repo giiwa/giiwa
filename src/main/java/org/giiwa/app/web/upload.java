3e8f6240a99e27fb6907c3b40f4a84e652c9b4b3
joe <joe@192.168.1.161>
2017-09-27 11:32:47 +0800

    add "state" in repo.java

--- src/main/java/org/giiwa/app/web/upload.java
+++ src/main/java/org/giiwa/app/web/upload.java
@@ -19,6 +19,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.fileupload.FileItem;
 import org.giiwa.core.bean.UID;
 import org.giiwa.core.bean.X;
+import org.giiwa.core.conf.Global;
 import org.giiwa.core.json.JSON;
 import org.giiwa.framework.bean.*;
 import org.giiwa.framework.web.*;
@@ -33,126 +34,132 @@ import org.giiwa.framework.web.*;
  */
 public class upload extends Model {
 
-  /*
-   * (non-Javadoc)
-   * 
-   * @see org.giiwa.framework.web.Model#onPost()
-   */
-  @Override
-  public void onPost() {
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.giiwa.framework.web.Model#onPost()
+	 */
+	@Override
+	public void onPost() {
 
-    JSON jo = new JSON();
-    User me = this.getUser();
+		JSON jo = new JSON();
+		User me = this.getUser();
 
-    if (me != null) {
-      // String access = Module.home.get("upload.require.access");
+		if (me != null) {
+			// String access = Module.home.get("upload.require.access");
 
-      FileItem file = this.getFile("file");
-      store(me.getId(), file, jo);
+			FileItem file = this.getFile("file");
+			store(me.getId(), file, jo);
 
-    } else {
-      this.set(X.ERROR, HttpServletResponse.SC_UNAUTHORIZED);
-      jo.put(X.MESSAGE, lang.get("login.required"));
-    }
+		} else {
+			this.set(X.ERROR, HttpServletResponse.SC_UNAUTHORIZED);
+			jo.put(X.MESSAGE, lang.get("login.required"));
+		}
 
-    // /**
-    // * test
-    // */
-    // jo.put("error", "error");
-    this.response(jo);
+		// /**
+		// * test
+		// */
+		// jo.put("error", "error");
+		this.response(jo);
 
-  }
+	}
 
-  private boolean store(long me, FileItem file, JSON jo) {
-    String tag = this.getString("tag");
+	private boolean store(long me, FileItem file, JSON jo) {
+		String tag = this.getString("tag");
 
-    try {
-      String range = this.getHeader("Content-Range");
-      if (range == null) {
-        range = this.getString("Content-Range");
-      }
-      long position = 0;
-      long total = 0;
-      String lastModified = this.getHeader("lastModified");
-      if (X.isEmpty(lastModified)) {
-        lastModified = this.getString("lastModified");
-      }
-      if (X.isEmpty(lastModified)) {
-        lastModified = this.getString("lastModifiedDate");
-      }
+		try {
+			String range = this.getHeader("Content-Range");
+			if (range == null) {
+				range = this.getString("Content-Range");
+			}
+			long position = 0;
+			long total = 0;
+			String lastModified = this.getHeader("lastModified");
+			if (X.isEmpty(lastModified)) {
+				lastModified = this.getString("lastModified");
+			}
+			if (X.isEmpty(lastModified)) {
+				lastModified = this.getString("lastModifiedDate");
+			}
 
-      if (range != null) {
+			if (range != null) {
 
-        // bytes 0-9999/22775650
-        String[] ss = range.split(" ");
-        if (ss.length > 1) {
-          range = ss[1];
-        }
-        ss = range.split("-|/");
-        if (ss.length == 3) {
-          position = X.toLong(ss[0]);
-          total = X.toLong(ss[2]);
-        }
+				// bytes 0-9999/22775650
+				String[] ss = range.split(" ");
+				if (ss.length > 1) {
+					range = ss[1];
+				}
+				ss = range.split("-|/");
+				if (ss.length == 3) {
+					position = X.toLong(ss[0]);
+					total = X.toLong(ss[2]);
+				}
 
-        // log.debug(range + ", " + position + "/" + total);
-      }
+				// log.debug(range + ", " + position + "/" + total);
+			}
 
-      String id = UID.id(me, tag, file.getName(), total, lastModified);
+			String id = UID.id(me, tag, file.getName(), total, lastModified);
 
-      log.debug("storing, id=" + id + ", name=" + file.getName() + ", tag=" + tag + ", total=" + total + ", last="
-          + lastModified);
+			log.debug("storing, id=" + id + ", name=" + file.getName() + ", tag=" + tag + ", total=" + total + ", last="
+					+ lastModified);
 
-      String share = this.getString("share");
-      String folder = this.getString("folder");
+			String share = this.getString("share");
+			String folder = this.getString("folder");
 
-      long pos = Repo.store(folder, id, file.getName(), tag, position, total, file.getInputStream(), -1,
-          !"no".equals(share), me);
-      if (pos >= 0) {
-        if (jo == null) {
-          this.put("url", "/repo/" + id + "/" + file.getName());
-          this.put(X.ERROR, 0);
-          this.put("repo", id);
-          if (total > 0) {
-            this.put("name", file.getName());
-            this.put("pos", pos);
-            this.put("size", total);
-          }
-        } else {
-          jo.put("url", "/repo/" + id + "/" + file.getName());
-          jo.put("repo", id);
-          jo.put(X.ERROR, 0);
-          if (total > 0) {
-            jo.put("name", file.getName());
-            jo.put("pos", pos);
-            jo.put("size", total);
-          }
-        }
+			long pos = Repo.store(folder, id, file.getName(), tag, position, total, file.getInputStream(), -1,
+					!"no".equals(share), me);
+			if (pos >= 0) {
+				if (jo == null) {
+					this.put("url", "/repo/" + id + "/" + file.getName());
+					this.put(X.ERROR, 0);
+					this.put("repo", id);
+					if (total > 0) {
+						this.put("name", file.getName());
+						this.put("pos", pos);
+						this.put("size", total);
+					}
+				} else {
+					jo.put("url", "/repo/" + id + "/" + file.getName());
+					jo.put("repo", id);
+					jo.put(X.ERROR, 0);
+					if (total > 0) {
+						jo.put("name", file.getName());
+						jo.put("pos", pos);
+						jo.put("size", total);
+					}
+				}
 
-        // Session.load(sid()).set("access.repo." + id, 1).store();
-      } else {
-        if (jo == null) {
-          this.set(X.ERROR, HttpServletResponse.SC_BAD_REQUEST);
-          this.put(X.MESSAGE, lang.get("repo.locked"));
-        } else {
-          jo.put(X.ERROR, HttpServletResponse.SC_BAD_REQUEST);
-          jo.put(X.MESSAGE, lang.get("repo.locked"));
-        }
-        return false;
-      }
-      return true;
-    } catch (Exception e) {
-      log.error(e.getMessage(), e);
-      OpLog.error(upload.class, "", e.getMessage(), e, login, this.getRemoteHost());
+				jo.put(X.STATE, 200);
+				jo.put("site", Global.getString("site.url", X.EMPTY));
+				// Session.load(sid()).set("access.repo." + id, 1).store();
+			} else {
+				if (jo == null) {
+					this.set(X.ERROR, HttpServletResponse.SC_BAD_REQUEST);
+					this.put(X.MESSAGE, lang.get("repo.locked"));
+					this.put(X.STATE, HttpServletResponse.SC_BAD_REQUEST);
+				} else {
+					jo.put(X.ERROR, HttpServletResponse.SC_BAD_REQUEST);
+					jo.put(X.MESSAGE, lang.get("repo.locked"));
+					jo.put(X.STATE, HttpServletResponse.SC_BAD_REQUEST);
+				}
+				return false;
+			}
+			return true;
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
+			OpLog.error(upload.class, "", e.getMessage(), e, login, this.getRemoteHost());
 
-      if (jo == null) {
-        this.set(X.ERROR, HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-        this.put(X.MESSAGE, lang.get(e.getMessage()));
-      } else {
-        jo.put(X.ERROR, HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-        jo.put(X.MESSAGE, lang.get(e.getMessage()));
-      }
-    }
+			if (jo == null) {
+				this.set(X.ERROR, HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+				this.put(X.MESSAGE, lang.get(e.getMessage()));
+				this.put(X.STATE, HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			} else {
+				jo.put(X.ERROR, HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+				jo.put(X.MESSAGE, lang.get(e.getMessage()));
+				jo.put(X.STATE, HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			}
+		}
 
-    return false;
-  }
+		return false;
+	}
 }
