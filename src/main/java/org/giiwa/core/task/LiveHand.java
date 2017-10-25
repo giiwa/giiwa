116137a3b3a7e88736497059a22108878d34435d
joe <joe@joedembp>
2017-10-25 13:34:46 +0800

    update LiveHand.java

--- src/main/java/org/giiwa/core/task/LiveHand.java
+++ src/main/java/org/giiwa/core/task/LiveHand.java
@@ -3,16 +3,27 @@ package org.giiwa.core.task;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.giiwa.core.bean.TimeStamp;
 import org.giiwa.core.bean.X;
 
 public class LiveHand {
 
+	private Log log = LogFactory.getLog(LiveHand.class);
+
 	private boolean live = true;
 	private long count = 0;
 	private long max = 0;
 	private long timeout = 0;
+
+	private Lock lock = new ReentrantLock();
+	private Condition door = lock.newCondition();
+
 	private TimeStamp created = TimeStamp.create();
 
 	private transient Map<String, Object> attachs = new HashMap<String, Object>();
@@ -46,48 +57,66 @@ public class LiveHand {
 		return live && (timeout <= 0 || created.pastms() < timeout);
 	}
 
-	public synchronized void stop() {
-		live = false;
-		this.notifyAll();
-	}
-
-	public synchronized void hold() {
-		count++;
-		if (max > 0 && count > max) {
-			try {
-				this.wait(X.AMINUTE);
-			} catch (InterruptedException e) {
-				// ignore
-			}
+	public void stop() {
+		try {
+			live = false;
+			lock.lock();
+			door.signalAll();
+		} finally {
+			lock.unlock();
 		}
 	}
 
-	public synchronized void drop() {
-		count--;
-		this.notifyAll();
+	public void hold() {
+		try {
+			lock.lock();
+			count++;
+			if (max > 0 && count > max) {
+				door.wait(X.AMINUTE);
+			}
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
+		} finally {
+			lock.unlock();
+		}
 	}
 
-	public synchronized boolean await() throws InterruptedException {
+	public void drop() {
+		try {
+			lock.lock();
+			count--;
+			door.signalAll();
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	public boolean await() throws InterruptedException {
 		if (timeout <= 0) {
 			return await(Long.MAX_VALUE - created.pastms());
 		}
 		return await(timeout - created.pastms());
 	}
 
-	public synchronized boolean await(long timeout) throws InterruptedException {
+	public boolean await(long timeout) throws InterruptedException {
 
 		TimeStamp t = TimeStamp.create();
 
 		long t1 = timeout - t.pastms();
-		while (isLive()) {
-			if (count <= 0)
-				return true;
-			this.wait(t1);
-			t1 = timeout - t.pastms();
-			// System.out.println("count=" + count);
+		try {
+			lock.lock();
+			while (t1 > 0 && isLive()) {
+				if (count <= 0)
+					return true;
+				
+				door.wait(t1);
+				t1 = timeout - t.pastms();
+				// System.out.println("count=" + count);
+			}
+		} finally {
+			lock.unlock();
 		}
 		return count <= 0;
-
 	}
 
 	@Override
