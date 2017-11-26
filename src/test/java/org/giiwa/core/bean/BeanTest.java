75792f8d0230027f1936e831d8272a6708df7b40
joe <joe@192.168.1.3>
2017-11-26 10:38:23 +0800

    add junit test

---
+++ src/test/java/org/giiwa/core/bean/BeanTest.java
@@ -0,0 +1,21 @@
+package org.giiwa.core.bean;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BeanTest {
+
+	@Test
+	public void test() {
+		// fail("Not yet implemented");
+	}
+
+	static class A extends Bean {
+
+	}
+
+	static class B extends A {
+
+	}
+}
