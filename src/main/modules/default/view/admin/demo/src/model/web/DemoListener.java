390397838462b339b3697bc324809fc2b508ab4a
giiwa <joe.j@giiwa.org>
2019-08-28 15:19:46 +0800

    fix the bug in demo

--- src/main/modules/default/view/admin/demo/src/model/web/DemoListener.java
+++ src/main/modules/default/view/admin/demo/src/model/web/DemoListener.java
@@ -1,6 +1,6 @@
 package org.giiwa.demo.web;
 
-import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration2.Configuration;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.giiwa.framework.bean.License;
@@ -12,7 +12,7 @@ public class DemoListener implements IListener {
 	static Log log = LogFactory.getLog(DemoListener.class);
 
 	public void onInit(Configuration conf, Module m) {
-		log.info("wedemo is initing...");
+		log.info("webdemo is initing...");
 
 		m.setLicense(License.LICENSE.free, "modulecode");
 
@@ -21,14 +21,14 @@ public class DemoListener implements IListener {
 	@Override
 	public void onStart(Configuration conf, Module m) {
 		// TODO Auto-generated method stub
-		log.info("wedemo is starting ...");
+		log.info("webdemo is starting ...");
 
 	}
 
 	@Override
 	public void onStop() {
 		// TODO Auto-generated method stub
-		log.info("wedemo is stopping ...");
+		log.info("webdemo is stopping ...");
 
 	}
 
