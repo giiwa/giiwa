36570e90ac0d7757425defa88e7ed6d4d39314af
giiwa <joe.j@giiwa.org>
2019-09-29 15:57:52 +0800

    update the windows shell

--- tools/giiwa.bat
+++ tools/giiwa.bat
@@ -1,13 +1,25 @@
 @echo off
 
-if not "%JAVA_HOME%" == "" goto doStart
+set daemon=%~dp0
+
+if not "%JAVA_HOME%" == "" goto checking
 echo ERROR, not set JAVA_HOME
 goto end
 
-:doStart
+:checking
+wmic process where caption="daemon.exe" get commandline /value | findstr "%daemon%" >NUL
+if ERRORLEVEL 1 goto starting
+if ERRORLEVEL 0 goto started
+goto end
+
+:starting
 echo %JAVA_HOME%
 java -version
 start %~dp0\daemon.exe
 echo giiwa is started.
+goto end
+
+:started
+echo giiwa already running. skipped
 
 :end
