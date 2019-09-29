8b20ddfcd8b34bf4a7e6acdbbe883d3a4fe8937a
giiwa <joe.j@giiwa.org>
2019-09-29 13:48:58 +0800

    update the windows shell

--- tools/giiwa.bat
+++ tools/giiwa.bat
@@ -1,4 +1,13 @@
 @echo off
 
+if not "%JAVA_HOME%" == "" goto doStart
+echo ERROR, not set JAVA_HOME
+goto end
+
+:doStart
+echo %JAVA_HOME%
+java -version
 call %~dp0\daemon.exe
 echo giiwa is started.
+
+:end
