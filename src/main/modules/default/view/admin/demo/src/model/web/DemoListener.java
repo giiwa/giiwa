8ad820fae08c64eea98c706143eb831221cf4408
giiwa <joe.j@giiwa.org>
2020-03-05 05:37:54 +0800

    release 2.1

--- src/main/modules/default/view/admin/demo/src/model/web/DemoListener.java
+++ src/main/modules/default/view/admin/demo/src/model/web/DemoListener.java
@@ -15,8 +15,7 @@ public class DemoListener implements IListener {
 	public void onInit(Configuration conf, Module m) {
 		log.warn("webdemo is initing...");
 
-		m.setLicense(License.LICENSE.free,
-				"ApATTsoSvCA8q8cg9TGS5JHVKwrxIO91l7i3ExAp7qHi83RsGR8njf4cdFjr981SLg0IL0EWM8f/9g4+CqcTAp1Ui4DWxA/ssEzDIt2hOw2vCLVwDv+N6yKPycwfYB0gjweemYbjhJIeeYjNK+KRC/badN2+/TQxxxxgqjMrlAU=");
+		m.setLicense(License.LICENSE.free, "modulecode");
 
 	}
 
